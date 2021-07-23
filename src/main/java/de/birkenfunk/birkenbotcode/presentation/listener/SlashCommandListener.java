b87b26429e0e2560fad3aee66f99e882f4a97bff
Birkenfunk <birkenfunk@outlook.de>
2021-07-23 09:04:39 +0200

    Added new Class for the SlashCommandListener

---
+++ src/main/java/de/birkenfunk/birkenbotcode/presentation/listener/SlashCommandListener.java
@@ -0,0 +1,14 @@
+package de.birkenfunk.birkenbotcode.presentation.listener;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SlashCommandListener extends ListenerAdapter{
+	
+	@Override
+	public void onSlashCommand(SlashCommandEvent event) {
+		
+	}
+
+}
