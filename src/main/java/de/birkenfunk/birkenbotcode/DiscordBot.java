23c3f1e45a2c8d20c1fc9a5352cb253920508072
Birkenfunk <birkenfunk@outlook.de>
2021-10-10 12:29:06 +0200

    Adding all commands, witch are in the DB but not in the DC

--- src/main/java/de/birkenfunk/birkenbotcode/DiscordBot.java
+++ src/main/java/de/birkenfunk/birkenbotcode/DiscordBot.java
@@ -3,6 +3,7 @@ package de.birkenfunk.birkenbotcode;
 import de.birkenfunk.birkenbotcode.application.IDatabase;
 import de.birkenfunk.birkenbotcode.common.Config;
 import de.birkenfunk.birkenbotcode.domain.CommandDTO;
+import de.birkenfunk.birkenbotcode.presentation.Mapper;
 import de.birkenfunk.birkenbotcode.presentation.listener.EventListener;
 import de.birkenfunk.birkenbotcode.presentation.listener.MessageListener;
 import de.birkenfunk.birkenbotcode.presentation.listener.ReactionListener;
@@ -14,6 +15,7 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import one.util.streamex.StreamEx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,49 +24,54 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.security.auth.login.LoginException;
+import java.util.List;
 
 /**
  * Main class which starts the Discord Bot
+ *
  * @author Alexander Asbeck
  * @version 1.4
  */
 @SpringBootApplication(scanBasePackages = {"de.birkenfunk.birkenbotcode"})
 public class DiscordBot {
-	private static final Logger LOGGER = LogManager.getLogger(DiscordBot.class);
-	private static JDA shardman;
+    private static final Logger LOGGER = LogManager.getLogger(DiscordBot.class);
+    private static JDA shardman;
 
-	@Autowired
-	private IDatabase database;
+    @Autowired
+    private IDatabase database;
 
-	public static void main(String[] args)  {
-		SpringApplication.run(DiscordBot.class, args);
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			LOGGER.info("Shutdown");
-			shardman.shutdown();
-		}));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBot.class, args);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Shutdown");
+            shardman.shutdown();
+        }));
+    }
 
 
-	@Bean
-	void startBot() throws LoginException {
-		JDABuilder builder = JDABuilder.createDefault(Config.getConfig().getToken());
-		builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
-		builder.setMemberCachePolicy(MemberCachePolicy.ALL);
-		builder.setStatus(OnlineStatus.ONLINE);
-		builder.setActivity(Activity.watching(Config.getConfig().getStatus()));
-		builder.addEventListeners(
-				new MessageListener(),
-				new ReactionListener(), 
-				new EventListener(), 
-				new SlashCommandListener(database));
-		shardman= builder.build();
-		for (CommandDTO command : database.getCommands()) {
-			if (command.isServerCommand()) {
-				//TODO .options einfügen
-				//shardman.upsertCommand(command.getName(), command.getDescription()).queue();
-			}
-		}
-		LOGGER.info("Bot online");
+    @Bean
+    void startBot() throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault(Config.getConfig().getToken());
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching(Config.getConfig().getStatus()));
+        builder.addEventListeners(
+                new MessageListener(),
+                new ReactionListener(),
+                new EventListener(),
+                new SlashCommandListener(database));
+        shardman = builder.build();
+        for (CommandDTO command : database.getCommands()) {
+            if (command.isServerCommand()) {
+                List<CommandDTO> commandList = StreamEx.of(shardman.retrieveCommands().complete())//
+                        .map(Mapper.commandToCommandDTO)
+                        .remove(it -> database.getCommands().contains(it))
+                        .toList();
+				StreamEx.of(commandList).forEachOrdered(it -> shardman.upsertCommand(it.getName(), it.getDescription()).addOptions().queue());
+            }
+        }
+        LOGGER.info("Bot online");
 
-	}
+    }
 }
