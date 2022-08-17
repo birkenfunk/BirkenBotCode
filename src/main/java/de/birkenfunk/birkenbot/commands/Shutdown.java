2849ca03dbb5e8b906854cb31b347498a8609370
Birkenfunk <birkenfunk@outlook.de>
2022-08-17 23:28:26 +0200

    Checking if User has Adminstation Rights

--- src/main/java/de/birkenfunk/birkenbot/commands/Shutdown.java
+++ src/main/java/de/birkenfunk/birkenbot/commands/Shutdown.java
@@ -1,10 +1,14 @@
 package de.birkenfunk.birkenbot.commands;
 
+import de.birkenfunk.birkenbot.ConfigReader;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import one.util.streamex.StreamEx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Set;
 
 public class Shutdown extends ListenerAdapter {
 
@@ -12,10 +16,22 @@ public class Shutdown extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.isFromType(ChannelType.PRIVATE) && event.getMessage().getContentRaw().equals("!shutdown")) {
-            log.debug("Shutdown command received");
-            event.getMessage().reply("Shutting down...").queue();
-            event.getJDA().shutdown();
+        if (event.getAuthor().isBot() || event.getChannelType() != ChannelType.PRIVATE || !event.getMessage().getContentRaw().startsWith("!")) {
+            return;
+        }
+        log.trace("Received message: {}", event.getMessage().getContentDisplay());
+        boolean isAdmin = StreamEx.of(ConfigReader.getAdminUserIDs()).has(String.valueOf(event.getAuthor().getIdLong()));
+        if (event.getMessage().getContentRaw().equals("!shutdown")) {
+            if (isAdmin) {
+                log.debug("Shutdown command received");
+                event.getMessage().reply("Shutting down...").queue();
+                event.getJDA().shutdown();
+            } else {
+                log.debug("User {} Tryed to Use an Admin Command", event.getAuthor().getName());
+                event.getMessage().reply("You are not allowed to use this command.").queue();
+            }
+        }else {
+            event.getMessage().reply("Possible Commands: !shutdown").queue();
         }
     }
 }
